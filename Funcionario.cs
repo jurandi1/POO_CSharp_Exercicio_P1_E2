6a37a8ca05f0f0fea8dfdbc260b9f200329f7d92
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 18:00:05 -0300

    Exercício concluído

---
+++ Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_CSharp_Exercicio_P1_E2
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
